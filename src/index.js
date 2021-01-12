477be1d4405027f5132653bbe73c49536c430cc3
Daniel Ronan <61019884+DcRonan@users.noreply.github.com>
2021-01-12 08:36:01 +0000

    Initialize development

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'phaser';
+import config from './config/config';
+import GameScene from './scenes/GameScene';
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
+}
+
+window.game = new Game();
