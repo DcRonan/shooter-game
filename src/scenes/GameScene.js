cfcd0962995f4e27064cdaa98576ce2d569f9721
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-13 14:22:53 +0000

    add current score display in game scence and menu button

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,15 +1,35 @@
 import Phaser from 'phaser';
+import Button from '../components/Button';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
-    super({ key: 'Game', active: true });
-  }
-
-  preload() {
-    // this.load.image('logo', 'assets/logo.png');
+    super('Game');
   }
 
   create() {
-    // this.add.image(400, 300, 'logo');
+    this.data.set('lives', 3);
+    this.data.set('level', 5);
+    this.data.set('score', 2000);
+
+    var text = this.add.text(40, 40, '', {
+      font: '24px Courier',
+      fill: '#00ff00',
+    });
+
+    text.setText([
+      'Level: ' + this.data.get('level'),
+      'Lives: ' + this.data.get('lives'),
+      'Score: ' + this.data.get('score'),
+    ]);
+
+    this.menuButton = new Button(
+      this,
+      400,
+      500,
+      'blueButton1',
+      'blueButton2',
+      'Menu',
+      'Title'
+    );
   }
 }
