58d07c8c1019f927bc7a13f3587492ee349b22a8
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-19 23:35:13 +0000

    add 6 test cases

--- test/index.test.js
+++ test/index.test.js
@@ -4,10 +4,6 @@ it('checks if the class Game is defined', () => {
   expect(Game).toBeDefined();
 });
 
-it('Base is a subclass of Phaser.GameObjects.Sprite', () => {
-  expect(Base).toBeSubclassOf(Phaser.GameObjects.Sprite);
-});
-
-it('Base has a constructor', () => {
-  expect(Base.prototype.constructor).not.toBe(false);
+it('Game has a constructor', () => {
+  expect(Game.prototype.constructor).not.toBe(false);
 });
