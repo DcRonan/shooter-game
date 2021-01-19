0d55ffeb195136e58fcc6f6b319dc58606283144
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-19 14:08:17 +0000

    add menu btn in leaderboard scene

--- src/scenes/LeaderBoard.js
+++ src/scenes/LeaderBoard.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import config from '../config/config';
 import { getScores, url } from '../components/api'
+import Button from '../components/Button'
 
 export default class LeaderBoard extends Phaser.Scene {
   constructor() {
@@ -21,6 +22,16 @@ export default class LeaderBoard extends Phaser.Scene {
           const userData = arr[i]
           this.add.text(config.width * 0.5 , config.height * 0.3 + 30 * i, `${userData.user}: ${userData.score}`).setOrigin()
         }
-      })  
+      })
+
+      this.menuButton = new Button(
+        this,
+        400,
+        500,
+        'blueButton1',
+        'blueButton2',
+        'Menu',
+        'Title'
+      );
   }
 }
