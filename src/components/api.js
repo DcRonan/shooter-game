f88c983a73278b8fe682884e7bb57c4b2284826a
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-20 07:38:55 +0000

    add Error handling to api functions as the API server is currently working

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,16 @@
 const key = 'DaHJ4cc1qLowKo5QQm9a';
-const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores`;
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DaHJ4cc1qLowKo5QQm9a/scores`;
+
+const networkError = (result) => {
+  if (!result.ok) {
+    throw new Error(result.statusText);
+  }
+  return result;
+};
 
 const getScores = () => {
   const response = fetch(url)
+    .then(networkError)
     .then((response) => response.json())
     .then((data) => {
       if (data.result.length === 0) {
@@ -10,7 +18,6 @@ const getScores = () => {
       }
       return data.result.sort((a, b) => b.score - a.score);
     })
-    .catch((error) => error);
   return response;
 };
 
@@ -19,6 +26,6 @@ const postScores = (user, score, url) => fetch(url, {
   mode: 'cors',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({ user, score }),
-});
+}).then(networkError);
 
 export { url, getScores, postScores };
